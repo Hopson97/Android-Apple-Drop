ef461c74b4b7c0c1ddf4d839ca2db88216bf7379
Hopson97 <mhopson@hotmail.co.uk>
2017-12-01 17:24:55 +0000

    android sprite now draws

--- scripts/common.py
+++ scripts/common.py
@@ -1,5 +1,9 @@
 import graphics as gfx
 
+import os
+
+FILE_DIR = os.path.dirname('__file__')
+
 UPDATE_SPEED = 30
  
 def getKeyPress(window):
@@ -12,3 +16,22 @@ def undrawAll(window):
 def switchState(window, control, newState):
     undrawAll(window)
     control["state"] = newState
+
+def getFileName(path, name):
+    newName = os.path.join(FILE_DIR, path + name)
+    return    os.path.abspath(os.path.realpath(newName))
+
+def loadSpriteVerticies(fileName):
+    fileName = getFileName("res/", fileName + ".txt")
+    with open(fileName) as inFile:
+        data = inFile.read()
+    data = data.split()
+    data = list(map(int, data))
+
+    points = [] 
+    for i in range(0, len(data), 2):
+        x = data[i]
+        y = data[i + 1]
+        points.append(gfx.Point(x, y))
+    return points
+
--- scripts/state_playing.py
+++ scripts/state_playing.py
@@ -3,27 +3,27 @@ import state_enum as states
 
 from common import *
 
-#from original code by Dr. Poole
+#based on the original "android.py" code by Dr. Poole
 def createPlayerSprite(window):
-    head = gfx.Circle(gfx.Point(0.5, 0.5), 0.03)
-    body = gfx.Rectangle(gfx.Point(0.47, 0.45), gfx.Point(0.53, 0.5))
-    leg1 = gfx.Rectangle(gfx.Point(0.48, 0.42), gfx.Point(0.49, 0.45))
-    leg2 = gfx.Rectangle(gfx.Point(0.52, 0.42), gfx.Point(0.51, 0.45))
-    arm1 = gfx.Rectangle(gfx.Point(0.53, 0.46), gfx.Point(0.54, 0.5))
-    arm2 = gfx.Rectangle(gfx.Point(0.46, 0.46), gfx.Point(0.47, 0.5))
-    eye1 = gfx.Circle(gfx.Point(0.49, 0.51), 0.005)
-    eye2 = gfx.Circle(gfx.Point(0.51, 0.51), 0.005)
-    android = [head, body, leg1, leg2, 
-               arm1, arm2, eye1, eye2]
-    for part in android:
-        if part in [eye1, eye2]:
+
+    coords = loadSpriteVerticies("android")
+    body = gfx.Polygon(coords).draw(window)
+    head = gfx.Circle(gfx.Point(45, 20), 30).draw(window)
+    eye1 = gfx.Circle(gfx.Point(30, 5), 5).draw(window)
+    eye2 = gfx.Circle(gfx.Point(60, 5), 5).draw(window)
+
+    droidParts = [body, head, eye1, eye2]
+    eyes = [eye1, eye2]
+    for part in droidParts:
+        if part in eyes:
             colour = "white"
         else:
             colour = "green"
         part.setFill(colour)
         part.setOutline(colour)
-        part.draw(window)
-    return android
+        part.move(100, 100)
+
+    return body
 
 def movePlayer(x, playerSprite):
     for part in playerSprite:
@@ -31,13 +31,13 @@ def movePlayer(x, playerSprite):
 
 
 def runPlayState(window, control):
-    print(len(window.items))
+
     playerSprite = createPlayerSprite(window)
-    print(len(window.items))
+
     while True:
         key = getKeyPress(window)
 
-        print(len(window.items))
+
         if key == "a":
             movePlayer(-1, playerSprite)
         elif key == "d":
