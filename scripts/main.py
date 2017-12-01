4aa48809aaa467cd2d2b17ed596002f50fcbef17
Hopson97 <mhopson@hotmail.co.uk>
2017-12-01 08:51:23 +0000

    Start creation of game play

--- scripts/main.py
+++ scripts/main.py
@@ -2,14 +2,14 @@ import graphics as gfx
 import state_enum as states
 
 import state_playing
-import state_menu 
-import state_credits 
+import state_menu
+import state_credits
 import state_splash
 
 gameRunning = True
 
 if __name__ == "__main__":
-    window = gfx.GraphWin("Apple Drop - By Matthew Hopson", 400, 400, autoflush = False)
+    window = gfx.GraphWin("Android Apple Drop - By Matthew Hopson", 1280, 720, autoflush = False)
 
     control = {"state": states.STATE_PLAYING}
 
@@ -24,8 +24,9 @@ if __name__ == "__main__":
         elif currentState == states.STATE_SPLASH:
             state_splash.runSplashState(window, control)  
         
-        if window.closed:
+        if window.closed or currentState == states.EXIT:
             gameRunning = False 
+            print("exit")
         gfx.update(30)
         
 
--- scripts/state_playing.py
+++ scripts/state_playing.py
@@ -3,16 +3,47 @@ import state_enum as states
 
 from common import *
 
+#from original code by Dr. Poole
+def createPlayerSprite(window):
+    head = gfx.Circle(gfx.Point(0.5, 0.5), 0.03)
+    body = gfx.Rectangle(gfx.Point(0.47, 0.45), gfx.Point(0.53, 0.5))
+    leg1 = gfx.Rectangle(gfx.Point(0.48, 0.42), gfx.Point(0.49, 0.45))
+    leg2 = gfx.Rectangle(gfx.Point(0.52, 0.42), gfx.Point(0.51, 0.45))
+    arm1 = gfx.Rectangle(gfx.Point(0.53, 0.46), gfx.Point(0.54, 0.5))
+    arm2 = gfx.Rectangle(gfx.Point(0.46, 0.46), gfx.Point(0.47, 0.5))
+    eye1 = gfx.Circle(gfx.Point(0.49, 0.51), 0.005)
+    eye2 = gfx.Circle(gfx.Point(0.51, 0.51), 0.005)
+    android = [head, body, leg1, leg2, 
+               arm1, arm2, eye1, eye2]
+    for part in android:
+        if part in [eye1, eye2]:
+            colour = "white"
+        else:
+            colour = "green"
+        part.setFill(colour)
+        part.setOutline(colour)
+        part.draw(window)
+    return android
+
+def movePlayer(x, playerSprite):
+    for part in playerSprite:
+        part.move(x, 0)
+
+
 def runPlayState(window, control):
-    rect = gfx.Rectangle(gfx.Point(0, 0), gfx.Point(20, 20))
-    rect.draw(window)
+    print(len(window.items))
+    playerSprite = createPlayerSprite(window)
+    print(len(window.items))
     while True:
         key = getKeyPress(window)
 
-        rect.move(1, 1)
 
-        if key == "n":
-            switchState(window, control, states.STATE_MENU)
+        if key == "a":
+            movePlayer(-1, playerSprite)
+        elif key == "d":
+            movePlayer(1, playerSprite)
+        elif key == "e":
+            switchState(window, control, states.EXIT)
             return
 
         gfx.update(30)
