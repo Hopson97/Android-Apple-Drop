17f9531811d0577405739f4640ecae17e01eb812
Hopson97 <mhopson@hotmail.co.uk>
2017-12-16 22:50:54 +0000

    Remove some inconsistant style

--- scripts/highscores.py
+++ scripts/highscores.py
@@ -6,40 +6,40 @@ from pathlib import Path
 import common
 
 #Relative path to the text file containing the highscores listings
-_PATH = "../res/highscores.txt"
+PATH = "../res/highscores.txt"
 
 #Constants for the creation of the highscore GUI
-_GAP = common.WINDOW_WIDTH / 4
-_RANK_X_LOCATION  = _GAP
-_NAME_X_LOCATION  = _GAP * 2
-_SCORE_X_LOCATION = _GAP * 3
+GAP = common.WINDOW_WIDTH / 4
+RANK_X_LOCATION  = GAP
+NAME_X_LOCATION  = GAP * 2
+SCORE_X_LOCATION = GAP * 3
 
-def _highscoresExist():
-    path = Path(_PATH)
+def highscoresExist():
+    path = Path(PATH)
     return path.is_file()
 
 
-def _createFile():
-    open(_PATH, "w").close()
+def createFile():
+    open(PATH, "w").close()
 
 
-def _loadScores():
+def loadScores():
     '''Loads the raw data from the highscore file'''
-    if not _highscoresExist():
-        _createFile()
+    if not highscoresExist():
+        createFile()
 
-    with open(_PATH) as inFile:
+    with open(PATH) as inFile:
         data = inFile.read()
     return data
 
-def _writeScores(scores):
+def writeScores(scores):
     '''Writes list of tuple(name, score) to the highscore file'''
-    with open(_PATH, "w") as outFile:
+    with open(PATH, "w") as outFile:
         for nameScore in scores:
             #Writes the tuple to the file, with a space between the elements
             outFile.write(" ".join(str(x) for x in nameScore) + "\n")
 
-def _extractScores(data):
+def extractScores(data):
     '''Extracts the highscores, and returns a list of tuple(name, score)'''
     highscores = []
     data = data.split()
@@ -48,41 +48,41 @@ def _extractScores(data):
         highscores.append(pair)
     return highscores
 
-def _getScoresList():
-    return _extractScores(_loadScores())
+def getScoresList():
+    return extractScores(loadScores())
 
 def submitScore(name, score):
     '''Adds a score to the highscores'''
-    highscores = _getScoresList()
+    highscores = getScoresList()
     highscores.append((name, score))
     highscores = sorted(highscores, key = lambda x: x[1])
     highscores = highscores[::-1]
-    _writeScores(highscores)
+    writeScores(highscores)
 
-def _addHighscoreTitles(sprites, y):
+def addHighscoreTitles(sprites, y):
     '''Adds the titles to the highscores'''
-    sprites.append(gfx.Text(gfx.Point(_RANK_X_LOCATION,  y), "Rank"))
-    sprites.append(gfx.Text(gfx.Point(_NAME_X_LOCATION,  y), "Name"))
-    sprites.append(gfx.Text(gfx.Point(_SCORE_X_LOCATION, y), "Score"))
+    sprites.append(gfx.Text(gfx.Point(RANK_X_LOCATION,  y), "Rank"))
+    sprites.append(gfx.Text(gfx.Point(NAME_X_LOCATION,  y), "Name"))
+    sprites.append(gfx.Text(gfx.Point(SCORE_X_LOCATION, y), "Score"))
 
     for i in range(1, 4):
         sprites[-i].setStyle("bold")
         sprites[-i].setTextColor("gray10")
 
-def _createBackgroundRect(sprites, y, colour):
+def createBackgroundRect(sprites, y, colour):
     rect = gfx.Rectangle(gfx.Point(0, y - 10), gfx.Point(common.WINDOW_WIDTH, y + 10))
     rect.setFill(colour)
     rect.setOutline(colour)
     sprites.append(rect)
 
-def _addField(sprites, name, rank, score, y):
-    sprites.append(gfx.Text(gfx.Point(_RANK_X_LOCATION,  y), rank))
-    sprites.append(gfx.Text(gfx.Point(_NAME_X_LOCATION,  y), name))
-    sprites.append(gfx.Text(gfx.Point(_SCORE_X_LOCATION, y), score))
+def addField(sprites, name, rank, score, y):
+    sprites.append(gfx.Text(gfx.Point(RANK_X_LOCATION,  y), rank))
+    sprites.append(gfx.Text(gfx.Point(NAME_X_LOCATION,  y), name))
+    sprites.append(gfx.Text(gfx.Point(SCORE_X_LOCATION, y), score))
 
 def createHighscoresDisplay(window):
     '''Creation of the GUI for the highscores screen'''
-    highscores = _getScoresList()
+    highscores = getScoresList()
     sprites = []
 
     #Create title bar
@@ -94,11 +94,11 @@ def createHighscoresDisplay(window):
         name  = str(highscores[i - 1][0])
         score = str(highscores[i - 1][1])
         y     = i * 20 + 100 + 10
-        _createBackgroundRect(sprites, y, colours[i])
+        createBackgroundRect(sprites, y, colours[i])
         if i == 0:
-            _addHighscoreTitles(sprites, y)
+            addHighscoreTitles(sprites, y)
             continue
-        _addField(sprites, name, rank, score, y)
+        addField(sprites, name, rank, score, y)
         if i + 1 == 26: #Maximum of 25 highscore fields can be displayed
             break
 
--- scripts/projectile.py
+++ scripts/projectile.py
@@ -4,16 +4,16 @@ import common
 
 import apple as appleFuncs
 
-_SPEED = 10
+SPEED = 10
 
-def _testForAppleProjectileCollision(projectile, apples):
+def testForAppleProjectileCollision(projectile, apples):
     for apple in apples[:]:
         appleCenter = apple.getCenter()
         projCenter  = projectile.getCenter()
         if common.distanceBetween(appleCenter, projCenter) < appleFuncs.DIAMETER:
             appleFuncs.removeApple(apples, apple)
 
-def _moveProjectile(direction, projectile):
+def moveProjectile(direction, projectile):
     dx = direction.getX()
     dy = direction.getY()
     #direction.y += 0.05    #Apply gravity
@@ -23,8 +23,8 @@ def update(projectiles, projectileDirections, apples):
     '''Updates the player's projectiles'''
     removeMe = []
     for i in range(len(projectiles)):
-        _moveProjectile(projectileDirections[i], projectiles[i])
-        _testForAppleProjectileCollision(projectiles[i], apples)
+        moveProjectile(projectileDirections[i], projectiles[i])
+        testForAppleProjectileCollision(projectiles[i], apples)
         if projectiles[i].getCenter().y - appleFuncs.DIAMETER > common.WINDOW_HEIGHT:
             removeMe.append(i)
 
@@ -38,8 +38,8 @@ def create(playerPoint, target, window):
     proj = appleFuncs.makeDefaultApple(playerPoint.getX(), playerPoint.getY(), window)
 
     dirVector = common.normalise(gfx.Point(dx, dy))
-    dx = dirVector.getX() * _SPEED
-    dy = dirVector.getY() * _SPEED
+    dx = dirVector.getX() * SPEED
+    dy = dirVector.getY() * SPEED
     velocity = gfx.Point(dx, dy)
 
     return proj, velocity
